00c8124073df6fac952347b87a8486f34d65732d
Anthony Sneed <anthony.sneed@hilti.com>
2022-02-14 10:07:32 -0600

    Remove TestClient.

--- reference-architecture/TestClient/Configuration/OrderServiceSettings.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace TestClient.Configuration;
-
-public class OrderServiceSettings
-{
-    public string ServiceUri { get; set; } = null!;
-}
--- reference-architecture/TestClient/Configuration/SagaConfigServiceSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TestClient.Configuration;
-
-public class SagaConfigServiceSettings
-{
-    public string ServiceUri { get; set; } = null!;
-    public Guid SagaConfigId { get; set; }
-    public string SagaConfigPath { get; set; } = null!;
-    public bool Debug { get; set; }
-}
